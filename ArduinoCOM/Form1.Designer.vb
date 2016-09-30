77aa1a1c26aa3d7dfe112b5392552147f5416e7d
yangtang <yangtang@SJC-CND6282DJ7>
2016-09-30 13:35:33 -0700

    Add project files.

---
+++ ArduinoCOM/Form1.Designer.vb
@@ -0,0 +1,151 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.lstConsole = New System.Windows.Forms.ListBox()
+        Me.btnConnect = New System.Windows.Forms.Button()
+        Me.lstPorts = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.ni = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.inputText = New System.Windows.Forms.TextBox()
+        Me.Send = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'lstConsole
+        '
+        Me.lstConsole.BackColor = System.Drawing.Color.White
+        Me.lstConsole.FormattingEnabled = True
+        Me.lstConsole.ItemHeight = 16
+        Me.lstConsole.Location = New System.Drawing.Point(28, 193)
+        Me.lstConsole.Margin = New System.Windows.Forms.Padding(4)
+        Me.lstConsole.Name = "lstConsole"
+        Me.lstConsole.Size = New System.Drawing.Size(615, 308)
+        Me.lstConsole.TabIndex = 5
+        '
+        'btnConnect
+        '
+        Me.btnConnect.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnConnect.Location = New System.Drawing.Point(288, 69)
+        Me.btnConnect.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnConnect.Name = "btnConnect"
+        Me.btnConnect.Size = New System.Drawing.Size(127, 28)
+        Me.btnConnect.TabIndex = 7
+        Me.btnConnect.Text = "&Connect"
+        Me.btnConnect.UseVisualStyleBackColor = True
+        '
+        'lstPorts
+        '
+        Me.lstPorts.FormattingEnabled = True
+        Me.lstPorts.Location = New System.Drawing.Point(28, 69)
+        Me.lstPorts.Margin = New System.Windows.Forms.Padding(4)
+        Me.lstPorts.Name = "lstPorts"
+        Me.lstPorts.Size = New System.Drawing.Size(240, 24)
+        Me.lstPorts.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(25, 48)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(116, 17)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Select COM Port:"
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.Location = New System.Drawing.Point(31, 514)
+        Me.lblStatus.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(155, 17)
+        Me.lblStatus.TabIndex = 9
+        Me.lblStatus.Text = "Spotify is NOT running."
+        '
+        'ni
+        '
+        Me.ni.Icon = CType(resources.GetObject("ni.Icon"), System.Drawing.Icon)
+        Me.ni.Text = "ni"
+        Me.ni.Visible = True
+        '
+        'inputText
+        '
+        Me.inputText.Location = New System.Drawing.Point(28, 145)
+        Me.inputText.Multiline = True
+        Me.inputText.Name = "inputText"
+        Me.inputText.Size = New System.Drawing.Size(460, 22)
+        Me.inputText.TabIndex = 10
+        '
+        'Send
+        '
+        Me.Send.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Send.Location = New System.Drawing.Point(526, 143)
+        Me.Send.Name = "Send"
+        Me.Send.Size = New System.Drawing.Size(110, 24)
+        Me.Send.TabIndex = 11
+        Me.Send.Text = "Send"
+        Me.Send.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ClientSize = New System.Drawing.Size(669, 584)
+        Me.Controls.Add(Me.Send)
+        Me.Controls.Add(Me.inputText)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnConnect)
+        Me.Controls.Add(Me.lstPorts)
+        Me.Controls.Add(Me.lstConsole)
+        Me.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Spotify - Arduino Remote"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents lstConsole As System.Windows.Forms.ListBox
+    Friend WithEvents btnConnect As System.Windows.Forms.Button
+    Friend WithEvents lstPorts As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblStatus As System.Windows.Forms.Label
+    Friend WithEvents ni As System.Windows.Forms.NotifyIcon
+    Friend WithEvents inputText As TextBox
+    Friend WithEvents Send As Button
+    Friend WithEvents Timer2 As Timer
+End Class
---
+++ ArduinoCOM/Form1.vb
@@ -0,0 +1,185 @@
+﻿Imports System.IO
+Imports System.IO.Ports
+Imports System.Threading
+Imports Microsoft.VisualBasic.FileIO
+
+Public Class Form1
+    Dim currentSong As String
+    Dim buffer As String
+    Dim type As String = ""
+    Delegate Sub myMethodDelegate(ByVal [text] As String)
+    Dim hdlD As New myMethodDelegate(AddressOf processCommand)
+    Dim WithEvents SerialPort As New IO.Ports.SerialPort
+    Dim p() As Process
+
+    Private Sub Form1_Disposed(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Disposed
+        If SerialPort.IsOpen() Then
+            SerialPort.Close()
+        End If
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        ni.Visible = False
+        GetSerialPortNames()
+    End Sub
+
+    Sub GetSerialPortNames()
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            lstPorts.Items.Add(sp)
+        Next
+    End Sub
+
+    Sub sendserialdata(ByVal port As String, ByVal data As String)
+        If (SerialPort.IsOpen) Then
+            SerialPort.Write(data)
+        Else
+            MsgBox("not connected to port.")
+        End If
+    End Sub
+
+    Sub processCommand(ByVal myString As String)
+        buffer = buffer + myString  'reads register buffer
+        Dim str As String
+        str = buffer
+        If InStr(str, "|") Then
+            Dim words As String() = str.Split(New Char() {"|"}) 'Splits up string into words when "|" is seen
+            buffer = ""
+            Dim word As String
+            For Each word In words
+                If (word.Length > 0) Then
+                    Dim Spotify As New spotify()
+                    Select Case word
+                        Case "prev"
+                            Spotify.PlayPrev()
+                            lstConsole.Items.Add("Play previous song.")
+                            type = "prev"
+                        Case "next"
+                            Spotify.PlayNext()
+                            lstConsole.Items.Add("Play next song.")
+                            type = "next"
+                        Case "pause"
+                            Spotify.PlayPause()
+                            type = "play"
+                            'lstConsole.Items.Add("Spotify paused.")
+                        Case ""
+                            '  received an Unknown command. Deal with it.
+                            '  lstConsole.Items.Add("Received: " & word)
+                        Case Else
+                            lstConsole.Items.Add("Latency: " + words(0) + " ms")
+                            Create_CSV(type, words(0))
+                    End Select
+                End If
+            Next
+        End If
+
+    End Sub
+
+    Private Sub SerialPort_DataReceived(ByVal sender As Object, ByVal e As System.IO.Ports.SerialDataReceivedEventArgs) Handles SerialPort.DataReceived
+        Dim str As String = SerialPort.ReadExisting()
+
+        Invoke(hdlD, str)   'invoke processCommand method
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        checkSongStatus()
+    End Sub
+
+    Private Sub checkSongStatus()
+        p = Process.GetProcessesByName("Spotify")
+        If p.Count > 0 Then
+            lblStatus.Text = "Spotify is running."
+            Dim Spotify As New spotify()
+            If Spotify.Nowplaying() <> currentSong Then
+                currentSong = Spotify.Nowplaying()
+                If currentSong = "" Or currentSong = "Paused." Then
+                    lstConsole.Items.Add("Spotify Paused.")
+                Else
+                    lstConsole.Items.Add("Song changed to: " & currentSong)
+                    If (SerialPort.IsOpen()) Then
+                        SerialPort.Write(currentSong & vbCrLf)
+                    End If
+                End If
+            End If
+        Else
+            lblStatus.Text = "Spotify is NOT running."
+        End If
+    End Sub
+    Private Sub Form1_SizeChanged(ByVal sender As Object, ByVal e As System.EventArgs) _
+ Handles MyBase.SizeChanged
+
+        If Me.WindowState = FormWindowState.Minimized Then
+            Me.WindowState = FormWindowState.Minimized
+            Me.Visible = False
+            Me.ni.Visible = True
+        End If
+
+    End Sub
+    Private Sub ni_Click(ByVal sender As Object, ByVal e As System.EventArgs) _
+ Handles ni.Click
+
+        Me.Visible = True
+        Me.WindowState = FormWindowState.Normal
+        Me.ni.Visible = False
+
+    End Sub
+
+    Private Sub btnConnect_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnConnect.Click
+        If lstPorts.SelectedIndex <> -1 Then
+            Try
+                If SerialPort.IsOpen Then
+                    SerialPort.Close()
+                    btnConnect.Text = "Connect"
+                Else
+                    SerialPort.PortName = lstPorts.SelectedItem.ToString
+                    SerialPort.BaudRate = 9600
+                    SerialPort.DataBits = 8
+                    SerialPort.Parity = Parity.None
+                    SerialPort.StopBits = StopBits.One
+                    SerialPort.Handshake = Handshake.None
+                    SerialPort.Encoding = System.Text.Encoding.Default
+                    SerialPort.Open()
+                    btnConnect.Text = "Disconnect"
+                    lstConsole.Items.Add("press 1 then send to play/pause")
+                    lstConsole.Items.Add("press 2 then send for next song")
+                End If
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+        Else
+            MsgBox("Please choose a serial port", vbInformation, "Serial Port")
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Send.Click
+        If (SerialPort.IsOpen) Then
+            SerialPort.Write(inputText.Text)
+        Else
+            MsgBox("Not connected to port. Select COM port.")
+        End If
+    End Sub
+
+    Private Sub Create_CSV(ByVal _type As String, ByVal _latency As String)
+        Dim csvFile As String = "C:\Users\yangtang\Desktop\latencyData.csv"
+
+        If IO.File.Exists(csvFile) Then
+            Dim objWriter As New StreamWriter(csvFile, True)
+            objWriter.Write(DateTime.Now.ToString("MM/dd/yy") & ",")
+            objWriter.Write(DateTime.Now.ToString("hh:mm:ss tt") & ",")
+            objWriter.Write(_type & ",")
+            objWriter.Write(_latency)
+            objWriter.Write(Environment.NewLine)
+            objWriter.Close()
+        Else
+            Dim objWriter As IO.StreamWriter = IO.File.AppendText(csvFile)
+            objWriter.WriteLine("Date, Time, Command, Latency (ms)")
+            objWriter.Write(DateTime.Now.ToString("MM/dd/yy") & ",")
+            objWriter.Write(DateTime.Now.ToString("hh:mm:ss tt") & ",")
+            objWriter.Write(_type & ",")
+            objWriter.Write(_latency)
+            objWriter.Write(Environment.NewLine)
+            objWriter.Close()
+        End If
+
+        Console.WriteLine(My.Computer.FileSystem.ReadAllText(csvFile))
+    End Sub
+End Class
